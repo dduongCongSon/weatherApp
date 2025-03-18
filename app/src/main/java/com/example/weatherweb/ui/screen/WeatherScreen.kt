3a7e05ad95685f5c07704fcb4274272ebc057be2
dduongCongSon <156944788+dduongCongSon@users.noreply.github.com>
2025-03-18 16:23:22 +0700

    build: layout of weather app

---
+++ app/src/main/java/com/example/weatherweb/ui/screen/WeatherScreen.kt
@@ -0,0 +1,36 @@
+package com.example.weatherweb.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.weatherweb.ui.screen.components.ActionBar
+import com.example.weatherweb.ui.screen.components.DayliForecast
+import com.example.weatherweb.ui.theme.ColorBackground
+
+@Composable
+fun WeatherScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = ColorBackground
+    ) { paddings ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddings)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 10.dp
+                )
+        ) {
+            ActionBar()
+            Spacer(modifier = Modifier.height(12.dp))
+            DayliForecast()
+        }
+    }
+}
